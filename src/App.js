ac980134129e351bd9030a8cdaf7838cdac9ff7a
Mayank <mayank8328@gmail.com>
2024-03-19 16:34:05 +0530

    React Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import styles from './App.module.css';
 import Student from './components/Student';
 
 function App() {
   
   return (
     <>
-    <h1>This is my first react app.</h1>
+    <h1 id={styles.stylish}>My first react app !!</h1>
       <Student name = "Mayank" place ="Gurugram" rollno = {21}/>
       <Student name = "Shivank" place ="Bangalore" rollno = {20}/>
       <Student name = "Mamta" place ="Chandigarh" rollno = {17}/>
